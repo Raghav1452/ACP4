6837ff3a9f8be4bc5cb3f765294d18d6c0c0d1e4
Raghav1452 <rwakhare2013@gmail.com>
2025-02-13 21:39:53 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.print("Enter the movie that you want to watch: ");
+        String movie = scn.nextLine();
+        System.out.print("The movie " + movie + " has how many ratings?");
+        int ratings = scn.nextInt();
+
+        if(ratings == 5000){
+            System.out.println("The movie is good.");
+        } else if (ratings == 25000) {
+            System.out.println("You better watch that movie.");
+        } else if (ratings >= 45000 & ratings == 70000){
+            System.out.println("The best movie in the world.");
+        } else if (ratings == 75000){
+            System.out.println("Out of the world.");
+        }
+    }
+}
